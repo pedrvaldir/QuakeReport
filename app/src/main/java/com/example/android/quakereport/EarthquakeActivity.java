eebf3eada2196bd5644c893a9c70627687429c08
Valdir.Pedroso <pedrvaldir@gmail.com>
2018-01-11 22:22:18 -0200

    implementação Asynctask.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,12 @@ import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
     @Override
@@ -23,11 +30,8 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-
-
+        earthquakeAsyncTask task = new earthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
 
         /*
         ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
@@ -40,7 +44,56 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakes.add(new Earthquake("4,9", "Paris", "Aug 19, 2012"));
         earthquakes.add(new Earthquake("7.2", "San Francisco", "Oct 30, 2011"));
 */
-        final  EarthquakeAdapter eartquakeAdapter = new EarthquakeAdapter(this, earthquakes);
+
+
+    }
+
+    private class earthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+
+        /**
+         * Esse método é chamado por uma thread em segundo plano, então, podemos executar
+         * operações mais lentas como conexões de rede
+         * <p>
+         * Não é correto atualizar a UI usando uma operação em segundo plano, então, apenas
+         * retornamos {@link ArrayList<Earthquake>} como resultado.
+         */
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... urls) {
+
+            // Não execute o pedido se não houver URLs ou se for nula
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            // Execute a solicitação HTTP para dados de terremoto e processe a resposta.
+
+            ArrayList<Earthquake> result = QueryUtils.buscarDadosTerremoto(urls[0]);
+
+            return result;
+        }
+
+        /**
+         * Esse método é chamado na UI principal depois que o trabalho em segundo plano foi executado.
+         * <p>
+         * Está correto modificar a UI com esse método. Pegamos o objeto {@link ArrayList<Earthquake>} (que
+         * foi retornado do doInBackground()) e atualizamos a tela.
+         */
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> result) {
+
+            // Se não houver resultados, não faça nada.
+            if (result == null) {
+                return;
+            }
+
+            updateUi(result);
+        }
+
+    }
+
+    private void updateUi(ArrayList<Earthquake> ListEarthquake) {
+
+        final EarthquakeAdapter eartquakeAdapter = new EarthquakeAdapter(this, ListEarthquake);
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
@@ -50,23 +103,25 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakeListView.setAdapter(eartquakeAdapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                   @Override
-                   public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                                                      @Override
+                                                      public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
-                       // Achar o terremoto atual que foi clicado
-                       Earthquake currentEarthquake = eartquakeAdapter.getItem(position);
+                                                          // Achar o terremoto atual que foi clicado
+                                                          Earthquake currentEarthquake = eartquakeAdapter.getItem(position);
 
-                       // Converte o URL String em um objeto URI (para passar no construtor de Intent)
-                       Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                                                          // Converte o URL String em um objeto URI (para passar no construtor de Intent)
+                                                          Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
 
-                       // Cria um novo intent para visualizar a URI do earthquake
-                       Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                                                          // Cria um novo intent para visualizar a URI do earthquake
+                                                          Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
 
-                       // Envia o intent para lançar uma nova activity
-                       startActivity(websiteIntent);
+                                                          // Envia o intent para lançar uma nova activity
+                                                          startActivity(websiteIntent);
 
-                       }
-                   }
+                                                      }
+                                                  }
         );
     }
+
+
 }
--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -1,11 +1,23 @@
 package com.example.android.quakereport;
 
+import android.text.TextUtils;
+import android.util.EventLog;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+
+import static com.example.android.quakereport.EarthquakeActivity.LOG_TAG;
 
 /**
  * Helper methods related to requesting and receiving earthquake data from USGS.
@@ -81,4 +93,147 @@ public final class QueryUtils {
         return earthquakes;
 
     }
+
+    /**
+     * Consulte o conjunto de dados do USGS e devolva um objeto {@link ArrayList<Earthquake>} para representar um único terremoto.
+*/
+    public static ArrayList<Earthquake> buscarDadosTerremoto(String requisicaoUrl) {
+        // Cria um objeto URL
+        URL url = createUrl(requisicaoUrl);
+
+
+        String respostaJson = null;
+
+        // Execute a solicitação HTTP para o URL e receba uma resposta JSON de volta
+        try {
+            respostaJson = requisicaoHttp(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error closing input stream", e);
+        }
+
+        // crie uma lista de objetos de terremoto com os dados correspondentes.
+
+
+        // Extraia campos relevantes da resposta JSON e retorne arraylist {@link Event}
+
+        ArrayList<Earthquake> earthquakes = extrairArrayJson(respostaJson);
+
+        // Return the {@link Event}
+        return earthquakes;
+    }
+
+    /**
+     * Faça uma solicitação HTTP para o URL fornecido e devolva um String como a resposta dos terremotos.
+     */
+    private static String requisicaoHttp(URL url) throws IOException {
+        String respostaJson = "";
+
+        // Se o URL for nulo, volte mais cedo.
+        if (url == null) {
+            return respostaJson;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            //urlConnection.setReadTimeout(10000 /* milliseconds */);
+            //urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Se a solicitação foi bem sucedida (código de resposta 200),
+            //  então leia o fluxo de entrada e analise a resposta.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                respostaJson = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Código de resposta de erro: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problema ao recuperar os resultados do terremoto JSON.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return respostaJson;
+    }
+
+    /**
+     * Converta o {@link InputStream} em uma String que contém toda a resposta JSON do servidor.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * Retorna o novo URL do URL fornecido.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Retornar um objeto {@link ArrayList<Earthquake>}, analisando informações sobre o primeiro terremoto da seqüência de dados EarthquakeJSON.
+     */
+    private static ArrayList<Earthquake> extrairArrayJson(String earthquakeJSON) {
+
+
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(earthquakeJSON)) {
+            return null;
+        }
+
+        try {
+
+            //Crie um ArrayList vazio para que possamos começar a adicionar os terremotos
+            ArrayList<Earthquake> earthquakes = new ArrayList<>();
+
+            // Extraia campos relevantes da resposta JSON e retorne arraylist {@link Event}
+            JSONObject objetoRespostaJson = new JSONObject(earthquakeJSON);
+            JSONArray earthquakeArray = objetoRespostaJson.getJSONArray("features");
+
+            for (int i = 0; i < earthquakeArray.length(); i++) {
+                JSONObject currentEarthquake = earthquakeArray.getJSONObject(i);
+                JSONObject properties = currentEarthquake.getJSONObject("properties");
+
+                double magnitude = properties.getDouble("mag");
+                String location = properties.getString("place");
+                long time = properties.getLong("time");
+
+                // Extrai o valor da chave chamada "url"
+                String url = properties.getString("url");
+
+                Earthquake earthquake = new Earthquake(magnitude, location, time, url);
+                earthquakes.add(earthquake);
+            }
+
+            // Create a new {@link Event} object
+            return new ArrayList<Earthquake>(earthquakes);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+        }
+        return null;
+    }
+
 }
